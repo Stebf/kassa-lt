438589419c55328d7e58977ae519d382d6a6abaf
Stebf <stebf@mail.de>
2026-05-14 15:29:08 +0200

    Add dependency to the tauri store plugin Idea to use it for settings

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,6 +15,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .setup(|app| {
             let app_handle = app.handle();
             if cfg!(debug_assertions) {
