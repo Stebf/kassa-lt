9a2761feeef2285baed71931227e1c7464a417b1
Stebf <stebf@mail.de>
2026-05-15 17:52:02 +0200

    Add SettingsAdminModePanel and update SettingsPage to include admin mode functionality Closes #28

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,75 +1,19 @@
-import { useEffect, useState } from 'react';
-import { useAdmin } from '../context/AdminContext';
-import { TextField, Box, OutlinedInput, InputLabel, InputAdornment, IconButton, Switch} from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import { TextField, Box, Typography} from '@mui/material';
 
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function SettingsPanel() {
-    const { adminModeEnabled, setAdminMode } = useAdmin();
-    const [showPassword, setShowPassword] = useState(false);
-
-    const handleClickShowPassword = () => {
-        setShowPassword(!showPassword);
-    };
-
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
-
-    const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
-
-    const handleAdminChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        await setAdminMode(event.target.checked);
-    };
 
     return (
         <Box sx={{ p: 2 }}>
             {/* <Box>
-                <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-                <InputLabel htmlFor={`admin-input`}>Passwort</InputLabel>
-                <OutlinedInput
+                <TextField
                     disabled
-                    id={`admin-input`}
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton
-                        aria-label={
-                            showPassword ? 'hide the password' : 'display the password'
-                        }
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        onMouseUp={handleMouseUpPassword}
-                        edge="end"
-                        >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                    </InputAdornment>
-                    }
-                    label="Passwort"
+                    label="Datenbankpfad"
+                    defaultValue="C:/Users/Stefan/AppData/Roaming/kassa-lt/kassalt.db"
+                    fullWidth
                 />
-                </FormControl>
             </Box> */}
-            <Box>
-            <FormGroup>
-
-                <FormControlLabel
-                    control={<Switch 
-                        checked={adminModeEnabled}
-                        onChange={handleAdminChange}
-                        />}
-                    label="Admin Modus"
-                    labelPlacement='start'
-                    />
-
-            </FormGroup>
-            </Box>
+            Space for more setting soon...
         </Box>
     );
 }
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,11 +1,15 @@
 import { Box, Typography } from "@mui/material";
 import SettingsPanel from "../components/SettingsPanel";
+import { useAdmin } from '../context/AdminContext';
+import SettingsAdminModePanel from "../components/SettingsAdminModePanel";
 
 export default function SettingsPage() {
+    const { adminModeEnabled, setAdminMode } = useAdmin();
     return (
         <Box sx={{ width: "80%", maxWidth: "none", mx: "auto", mt: 4, p: 2 }}>
             <Typography variant="h5">Einstellungen</Typography>
-            <SettingsPanel />
+            <SettingsAdminModePanel />
+            {adminModeEnabled && <SettingsPanel />}
         </Box>
     );
 }
