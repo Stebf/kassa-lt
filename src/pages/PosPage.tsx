4cc9b9d87918af2059107ad2d5d6847c208251e2
Stebf <stebf@mail.de>
2026-05-24 12:02:02 +0200

    Changed Layout to adapt for larger number of products

--- src/pages/PosPage.tsx
+++ src/pages/PosPage.tsx
@@ -7,16 +7,18 @@ export default function PosPage() {
     <Box
       sx={{
         p: 2,
-        height: "calc(100vh - 64px)",
+        height: "calc(100dvh - 64px)",
         display: "grid",
         gridTemplateColumns: { xs: "1fr", md: "2fr 1fr" },
+        gridTemplateRows: { xs: "minmax(0, 1fr) auto", md: "1fr" },
         gap: 2,
+        overflow: "hidden",
       }}
     >
-      <Box>
+      <Box sx={{ minHeight: 0, overflowY: "auto" }}>
           <ProductGrid />
       </Box>
-      <Box>
+        <Box sx={{ minHeight: 0, display: "flex", width: "100%", minWidth: 0 }}>
           <CartPanel />
       </Box>
     </Box>
